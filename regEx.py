d91eede7be3c005f498d5718817b8e441ff2caaf
Patrick Debattista <patrick8379@dubaicollege.orgi>
2021-01-06 09:14:48 +0400

    Extension challenges + funtion that rejects invalid results

--- regEx.py
+++ regEx.py
@@ -2,9 +2,9 @@ import re
 
 def textMatch(text, pattern):
     if re.search(pattern, text):
-        return print(text, "is valid")
+        return print(text, "Found a match")
     else:
-        return print(text, "is not valid")
+        return print(text, "No match")
 
 print("expression ab+")
 pattern = "ab*"
@@ -26,3 +26,34 @@ pattern = "abbb?"
 textMatch("abbb", pattern)
 textMatch("abb", pattern)
 textMatch("ab", pattern)
+#-----EXTENSION CHALLENGES-----
+def textMatch2(text, pattern, rejectPattern):
+    if re.search(pattern, text):
+        if re.search(rejectPattern, text):
+            return print(text, "No match")
+        else:
+            return print(text, "Found a match")
+    else:
+        return print(text, "No match")
+print("Check string only contains a certain set of characters")
+pattern = "\w+"
+rejectPattern = "\W+"
+textMatch2("hello", pattern, rejectPattern)
+textMatch2("Hello", pattern, rejectPattern)
+textMatch2("Hello1", pattern, rejectPattern)
+textMatch2("Hello!", pattern, rejectPattern)
+print("One upper case letter followed by lower case letters")
+pattern = "[A-Z][a-z]+"
+rejectPattern = "[A-Z][a-z]*([A-Z]+|[0-9]+|\W+)+"
+textMatch2("Hello", pattern, rejectPattern)
+textMatch2("Hello!", pattern, rejectPattern)
+textMatch2("HellO", pattern, rejectPattern)
+print("Anything between a and b")
+pattern = "^a\w+b$"
+textMatch("abcdcb", pattern)
+textMatch("bcdcb", pattern)
+textMatch("abcdcba", pattern)
+print("Match a word at the beginning of a string")
+textMatch("Hello world", "Hello")
+textMatch("Hello world", "hello")
+textMatch("hello world", "Hello")

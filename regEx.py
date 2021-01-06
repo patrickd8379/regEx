14e61a020c4f8712f92dd166f13b86902c9bef4f
Patrick Debattista <patrick8379@dubaicollege.orgi>
2021-01-06 08:31:46 +0400

    Made it so only one textMatch function where pattern can be changed

--- regEx.py
+++ regEx.py
@@ -1,43 +1,28 @@
 import re
 
-def textMatch1(text):
-    pattern1 = 'ab+'
-    if re.search(pattern1, text):
-        return print(text, "is valid")
-    else:
-        return print(text, "is not valid")
-def textMatch2(text):
-    pattern2 = 'ab?'
-    if re.search(pattern2, text):
-        return print(text, "is valid")
-    else:
-        return print(text, "is not valid")
-def textMatch3(text):
-    pattern3 = 'abbb'
-    if re.search(pattern3, text):
-        return print(text, "is valid")
-    else:
-        return print(text, "is not valid")
-def textMatch4(text):
-    pattern4 = 'abbb?'
-    if re.search(pattern4, text):
+def textMatch(text, pattern):
+    if re.search(pattern, text):
         return print(text, "is valid")
     else:
         return print(text, "is not valid")
 
 print("expression ab+")
-textMatch1("ab")
-textMatch1("a")
-textMatch1("abc")
+pattern = "ab*"
+textMatch("ab", pattern)
+textMatch("a", pattern)
+textMatch("abc", pattern)
 print("expression ab?")
-textMatch2("ab")
-textMatch2("a")
-textMatch2("abb")
+pattern = "ab?"
+textMatch("ab", pattern)
+textMatch("a", pattern)
+textMatch("abb", pattern)
 print("expression abbb")
-textMatch3("abbb")
-textMatch3("abb")
-textMatch3("abbbb")
+pattern = "abbb"
+textMatch("abbb", pattern)
+textMatch("abb", pattern)
+textMatch("abbbb", pattern)
 print("expression abbb?")
-textMatch4("abbb")
-textMatch4("abb")
-textMatch4("ab")
+pattern = "abbb?"
+textMatch("abbb", pattern)
+textMatch("abb", pattern)
+textMatch("ab", pattern)
